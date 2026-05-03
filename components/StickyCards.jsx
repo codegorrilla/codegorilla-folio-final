735b3da09848526be37a2c15980dd4f4b1698e71
Sanjib Adhya <code.gorrilla@gmail.com>
2026-05-03 07:15:09 +0530

    completed section transition

--- components/StickyCards.jsx
+++ components/StickyCards.jsx
@@ -12,25 +12,50 @@ const StickyCards = () => {
   const container = useRef(null);
 
   //Sticky card animation
-  useGSAP(() => {
-    const stickyCards = document.querySelectorAll(".sticky-card");
+  useGSAP(
+    () => {
+      const stickyCards = document.querySelectorAll(".sticky-card");
 
-    stickyCards.forEach((card, index) => {
-      if (index < stickyCards.length - 1) {
-        ScrollTrigger.create({
-          trigger: card,
-          start: "top top",
-          endTrigger: stickyCards[stickyCards.length - 1],
-          end: "top top",
-          pin: true,
-          pinSpacing: false,
-        });
-      }
-    });
-  });
+      stickyCards.forEach((card, index) => {
+        // this pins all the cards except the last one
+        if (index < stickyCards.length - 1) {
+          ScrollTrigger.create({
+            trigger: card,
+            start: "top top",
+            endTrigger: stickyCards[stickyCards.length - 1],
+            end: "top top",
+            pin: true,
+            pinSpacing: false,
+          });
+        }
+
+        // this animates the cards i.e. scaling down and rotating
+        if (index < stickyCards.length - 1) {
+          ScrollTrigger.create({
+            trigger: stickyCards[index + 1],
+            start: "top bottom",
+            end: "top top",
+            onUpdate: (self) => {
+              const progress = self.progress;
+              const scale = 1 - progress * 0.25;
+              const rotation = (index % 2 === 0 ? 5 : -5) * progress;
+              const afterOpacity = progress;
+
+              gsap.set(card, {
+                scale: scale,
+                rotation: rotation,
+                "--after-opacity": afterOpacity,
+              });
+            },
+          });
+        }
+      });
+    },
+    { scope: container },
+  );
 
   return (
-    <section className="w-full h-full relative bg-white">
+    <section className="w-full h-full relative bg-white" ref={container}>
       <AboutPage className="w-full min-h-screen flex gap-3 p-20 relative bg-brand-dark text-white will-change-transform sticky-card">
         About
       </AboutPage>
